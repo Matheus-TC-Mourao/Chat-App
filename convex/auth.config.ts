2ea32ffcdfbd0457ad911467a6fd5467b469a239
Matheus Teixeira Câmara Mourão <matheusmouraotc@gmail.com>
2024-10-14 15:26:34 -0300

    First commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://key-barnacle-96.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
---
+++ providers/ConvexClientProvider.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import LoadingLogo from "@/components/shared/LoadingLogo";
+import { Button } from "@/components/ui/button";
+import { ClerkProvider, SignedOut, SignInButton, useAuth } from "@clerk/nextjs";
+import { Authenticated, AuthLoading, ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const CONVEX_URL = process.env.NEXT_PUBLIC_CONVEX_URL || "";
+
+const convex = new ConvexReactClient(CONVEX_URL);
+
+const ConvexClientProvider = ({ children }: Props) => {
+  return (
+    <ClerkProvider>
+      <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+        <Authenticated>{children} </Authenticated>
+        <header className="h-full w-full flex justify-center items-center">
+          <Button>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+          </Button>
+        </header>
+        <AuthLoading>
+          <LoadingLogo />
+        </AuthLoading>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
+
+export default ConvexClientProvider;
