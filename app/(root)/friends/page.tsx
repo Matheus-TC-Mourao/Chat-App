aa26434ed3d3dc14ed5f2e2fd755bab308961287
Matheus Teixeira Câmara Mourão <matheusmouraotc@gmail.com>
2024-10-22 18:01:23 -0300

    WIP Add Friends

--- app/(root)/friends/page.tsx
+++ app/(root)/friends/page.tsx
@@ -1,11 +1,14 @@
 import ConversationFallback from "@/components/shared/conversation/ConversationFallback";
 import ItemList from "@/components/shared/item-list/ItemList";
 import React from "react";
+import AddFriendDialog from "./_components/AddFriendDialog";
 
 const FriendsPage = () => {
   return (
     <>
-      <ItemList title="Friends">Friends Page</ItemList>
+      <ItemList title="Friends" action={<AddFriendDialog />}>
+        Friends Page
+      </ItemList>
       <ConversationFallback />
     </>
   );
